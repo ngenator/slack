b5d89d1eed7f6ee35e490a2c0efff9668a541336
Daniel Ng <ngenator@gmail.com>
2016-11-07 14:48:21 -0500

    Adding slash command stuff

--- slash-commands.go
+++ slash-commands.go
@@ -1,7 +1,60 @@
 package slack
 
+import (
+	"bytes"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"errors"
+)
+
+
+// SlashCommandResponse is a reply to a slash command
 type SlashCommandResponse struct {
-	Type        string               `json:"response_type,omitempty"`
-	Text        string               `json:"text,omitempty"`
+	Type        string        `json:"response_type,omitempty"`
+	Text        string        `json:"text,omitempty"`
+	ChannelID   string        `json:"channel,omitempty"`
+	Username    string        `json:"username,omitempty"`
+	IconURL     string        `json:"icon_url,omitempty"`
+	IconEmoji   string        `json:"icon_emoji,omitempty"`
+	UnfurlLinks bool          `json:"unfurl_links,omitempty"`
 	Attachments []*MessageAttachment `json:"attachments,omitempty"`
+}
+
+// SlashCommand is a slash command converted to json from application/x-www-form-urlencoded
+type SlashCommand struct {
+	Token       string `json:"token"`
+	TeamID      string `json:"team_id"`
+	TeamDomain  string `json:"team_domain"`
+	ChannelID   string `json:"channel_id"`
+	ChannelName string `json:"channel_name"`
+	UserID      string `json:"user_id"`
+	UserName    string `json:"user_name"`
+	Command     string `json:"command"`
+	Text        string `json:"text"`
+	ResponseURL string `json:"response_url"`
+}
+
+// RespondToSlashCommand responds to a slack slash command by posting json to its response url
+func RespondToSlashCommand(responseURL string, response *SlashCommandResponse) error {
+	body, err := json.Marshal(response)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post(responseURL, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(t))
+	}
+
+	return nil
 }
